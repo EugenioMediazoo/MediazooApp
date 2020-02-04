d3444d9279e6d2b5ca0fddd82cdc815bfff821d2
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-04 12:29:42 +0000

    [+] master upload 1

---
+++ Assets/Mediazoo/Scripts/CamGyro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamGyro : MonoBehaviour
+{
+    GameObject camParent;
+
+    void Start()
+    {
+        camParent = new GameObject("CamParent");
+        camParent.transform.position = this.transform.position;
+        this.transform.parent = camParent.transform;
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        camParent.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        this.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+    }
+}
