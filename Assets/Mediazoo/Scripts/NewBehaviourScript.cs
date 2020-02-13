f6a02aff2017341a760add80c0c1b8e91abbcda6
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-13 11:09:21 +0000

    [~] chat resize [~] font sharpness

---
+++ Assets/Mediazoo/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject Parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var _height = this.gameObject.GetComponent<RectTransform>().sizeDelta.y;
+
+        var PrarentWidth = Parent.GetComponent<RectTransform>().sizeDelta.x;
+
+        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(PrarentWidth, _height);
+        //Screen.width
+    }
+
+}
