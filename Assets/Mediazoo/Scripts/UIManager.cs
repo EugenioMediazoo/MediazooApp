08c98b4eace580ab57608bf4beabf4d9acb34a74
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-13 17:53:28 +0000

    [~]scrolling scene

---
+++ Assets/Mediazoo/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject ReminderMessages;
+
+    public void Awake()
+    {
+        ReminderMessages.SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reminders.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
