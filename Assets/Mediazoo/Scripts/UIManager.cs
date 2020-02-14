3b4fb7ea0ab9e73068e2d8ae07acfa4438f979d8
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-14 16:33:04 +0000

    [~] Gyro office interactive scene

--- Assets/Mediazoo/Scripts/UIManager.cs
+++ Assets/Mediazoo/Scripts/UIManager.cs
@@ -1,25 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
+    //scenes
+    //public string Onboarding;
+    //public string Office360;
+
+    //bar
+    public Button CalendarButton;
+    public Button Button360;
+
+    //BGCanvas
+    public GameObject HomeBG;
+    private CanvasGroup HomeBGAlpha;
+    public GameObject MonocromeBG;
+    private CanvasGroup MonocromeBGAlpha;
+    public GameObject SignInUp;
+    private CanvasGroup SignInUpAlpha;
+    public GameObject ScrollView;
+    private CanvasGroup ScrollViewAlpha;
 
     public GameObject ReminderMessages;
 
+
     public void Awake()
     {
-        ReminderMessages.SetActive(true);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Reminders.SetActive(false);
+        if(ReminderMessages != null)
+            ReminderMessages.SetActive(true);
+
+        HomeBGAlpha = HomeBG.GetComponent<CanvasGroup>();
+        MonocromeBGAlpha = MonocromeBG.GetComponent<CanvasGroup>();
+        SignInUpAlpha = SignInUp.GetComponent<CanvasGroup>();
+        ScrollViewAlpha = ScrollView.GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Onboarding")
+            CalendarButton.Select();
+        //else if (sceneName == "Office360")
+        //    CalendarButton.Select();
+    }
+
+    public void CalendarScene()
+    {
+        //SceneManager.LoadScene(Onboarding, LoadSceneMode.Single);
+        HomeBGAlpha.alpha = 1;
+        MonocromeBGAlpha.alpha = 1;
+        SignInUpAlpha.alpha = 1;
+        ScrollViewAlpha.alpha = 1;
+    }
+
+    public void SphereScene()
+    {
+        //SceneManager.LoadScene(Office360, LoadSceneMode.Single);
+        HomeBGAlpha.alpha = 0;
+        MonocromeBGAlpha.alpha = 0;
+        SignInUpAlpha.alpha = 0;
+        ScrollViewAlpha.alpha = 0;
     }
 }
