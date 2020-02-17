13e992dbfc2628da2c9ff47dbdff181d532067d1
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-17 17:44:17 +0000

    [~] UI movements [+] team content

--- Assets/Mediazoo/Scripts/UIManager.cs
+++ Assets/Mediazoo/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 //using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
@@ -13,7 +14,7 @@ public class UIManager : MonoBehaviour
 
     //bar
     public Button CalendarButton;
-    public Button Button360;
+    private bool CalendarSceneBool;
 
     //BGCanvas
     public GameObject HomeBG;
@@ -26,8 +27,7 @@ public class UIManager : MonoBehaviour
     private CanvasGroup ScrollViewAlpha;
 
     public GameObject ReminderMessages;
-
-
+    
     public void Awake()
     {
         if(ReminderMessages != null)
@@ -37,6 +37,8 @@ public class UIManager : MonoBehaviour
         MonocromeBGAlpha = MonocromeBG.GetComponent<CanvasGroup>();
         SignInUpAlpha = SignInUp.GetComponent<CanvasGroup>();
         ScrollViewAlpha = ScrollView.GetComponent<CanvasGroup>();
+        
+        CalendarSceneBool = true;
     }
 
     public void Start()
@@ -52,19 +54,45 @@ public class UIManager : MonoBehaviour
 
     public void CalendarScene()
     {
-        //SceneManager.LoadScene(Onboarding, LoadSceneMode.Single);
+        if (!CalendarSceneBool)
+        {
+            //SceneManager.LoadScene(Onboarding, LoadSceneMode.Single);
+            DOTween.To(() => MonocromeBGAlpha.alpha, x => MonocromeBGAlpha.alpha = x, 1, 0.2f).SetEase(Ease.InQuad);
+            StartCoroutine(FadeInCalendar());
+
+            CalendarSceneBool =! CalendarSceneBool;
+        }
+        else
+            return;
+    }
+
+    IEnumerator FadeInCalendar()
+    {
+        yield return new WaitForSeconds(0.2f);
         HomeBGAlpha.alpha = 1;
-        MonocromeBGAlpha.alpha = 1;
         SignInUpAlpha.alpha = 1;
         ScrollViewAlpha.alpha = 1;
     }
 
-    public void SphereScene()
+
+
+public void SphereScene()
     {
-        //SceneManager.LoadScene(Office360, LoadSceneMode.Single);
-        HomeBGAlpha.alpha = 0;
-        MonocromeBGAlpha.alpha = 0;
-        SignInUpAlpha.alpha = 0;
-        ScrollViewAlpha.alpha = 0;
+        if (CalendarSceneBool)
+        {
+            //SceneManager.LoadScene(Office360, LoadSceneMode.Single);
+            HomeBGAlpha.alpha = 0;
+            //MonocromeBGAlpha.alpha = 0;
+
+            DOTween.To(() => MonocromeBGAlpha.alpha, x => MonocromeBGAlpha.alpha = x, 0, 0.2f).SetEase(Ease.InQuad);
+
+            SignInUpAlpha.alpha = 0;
+            ScrollViewAlpha.alpha = 0;
+
+            CalendarSceneBool = !CalendarSceneBool;
+        }
+        else
+            return;
     }
+    
 }
---
+++ Assets/Mediazoo/Scripts/UIManagerDockBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+//using UnityEngine.EventSystems;
+
+public class UIManagerDockBar : MonoBehaviour
+{
+    //time var
+    [Range(0f, 10f)]
+    public float AnimSpeed;
+
+    //pos var
+    public GameObject OffScreenLeft;
+    public GameObject OnScreenLeft;
+    public GameObject ScreenCenter;
+    public GameObject OnScreenRight;
+    public GameObject OffScreenRight;
+
+    //screens
+    public GameObject CalendarContent;
+    public GameObject TeamConent;
+
+
+    public void Awake()
+    {
+        TeamConent.transform.DOMoveX(OffScreenRight.transform.position.x, 0);
+    }
+
+    public void CalendarCanvas()
+    {
+        CalendarContent.transform.DOMoveX(ScreenCenter.transform.position.x, AnimSpeed).SetEase(Ease.InOutCubic);
+        TeamConent.transform.DOMoveX(OffScreenRight.transform.position.x, AnimSpeed).SetEase(Ease.InOutCubic);
+    }
+
+    public void TeamCanvas()
+    {
+        CalendarContent.transform.DOMoveX(OffScreenLeft.transform.position.x, AnimSpeed).SetEase(Ease.InOutCubic);
+        TeamConent.transform.DOMoveX(OnScreenLeft.transform.position.x, AnimSpeed).SetEase(Ease.InOutCubic);
+    }
+
+}
