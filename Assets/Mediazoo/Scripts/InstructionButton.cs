7b7631aaeeebb31d5fc8bdeb99c5dfe71de16acf
EugenioMediazoo <eugenio@mediazoo.tv>
2020-02-20 18:07:41 +0000

    [+] instruction Marker

--- Assets/Mediazoo/Scripts/InstructionButton.cs
+++ Assets/Mediazoo/Scripts/InstructionButton.cs
@@ -13,6 +13,8 @@ public class InstructionButton : MonoBehaviour
     public GameObject CurrentCardPivot;
     private CanvasGroup CurrentCardPivotCG;
 
+    public GameObject Marker;
+
     //animation
     [Range(0.1f, 5)]
     public float animationSpeed = 1;
@@ -29,12 +31,19 @@ public class InstructionButton : MonoBehaviour
     {
         if (NextCardPivot != null)
         {
-            NextCardPivot.transform.DORotate(new Vector3(0, 0, 0), animationSpeed, RotateMode.Fast).SetEase(Ease.InOutCubic);
+            NextCardPivot.transform.DORotate(new Vector3(0, 0, 0), animationSpeed, RotateMode.Fast).SetEase(Ease.InOutBack);
             DOTween.To(() => NextCardPivotCG.alpha, x => NextCardPivotCG.alpha = x, 1, (animationSpeed/2)).SetEase(Ease.InCubic);
-
         }
 
-        CurrentCardPivot.transform.DORotate(new Vector3(0, 0, 25), animationSpeed, RotateMode.Fast).SetEase(Ease.InOutCubic);
+        CurrentCardPivot.transform.DORotate(new Vector3(0, 0, 25), animationSpeed, RotateMode.Fast).SetEase(Ease.InOutBack);
         DOTween.To(() => CurrentCardPivotCG.alpha, x => CurrentCardPivotCG.alpha = x, 0, animationSpeed).SetEase(Ease.InOutCubic).SetDelay(0.5f);
+
+        if (Marker != null)
+        {
+            var MarkerPos = Marker.transform.localPosition.x;
+            Marker.transform.DOLocalMoveX(MarkerPos + 80, animationSpeed).SetEase(Ease.InOutBack);
+        }
+        else
+            return;
     }
 }
--- Assets/Mediazoo/Scripts/SlideInInstruction.cs
+++ Assets/Mediazoo/Scripts/SlideInInstruction.cs
@@ -5,9 +5,12 @@ using DG.Tweening;
 
 public class SlideInInstruction : MonoBehaviour
 {
-    public bool low;
+    private bool low;
     public GameObject BlurBG;
     private CanvasGroup BlurBGCanvas;
+
+    public GameObject MarkerGroup;
+    private CanvasGroup MarkerGroupCanvas;
 
     //animation
     [Range(0.1f, 5)]
@@ -20,6 +23,9 @@ public class SlideInInstruction : MonoBehaviour
 
         BlurBGCanvas = BlurBG.GetComponent<CanvasGroup>();
         BlurBGCanvas.alpha = 0;
+
+        MarkerGroupCanvas = MarkerGroup.GetComponent<CanvasGroup>();
+        MarkerGroupCanvas.alpha = 0;
     }
 
     //void Start()
@@ -34,7 +40,9 @@ public class SlideInInstruction : MonoBehaviour
             this.transform.DOLocalMoveY(-10000f, 1).SetEase(Ease.InCubic);
             low = false;
 
-            DOTween.To(() => BlurBGCanvas.alpha, x => BlurBGCanvas.alpha = x, 1, (animationSpeed/2)).SetEase(Ease.InCubic);
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Append(DOTween.To(() => BlurBGCanvas.alpha, x => BlurBGCanvas.alpha = x, 1, (animationSpeed / 2)).SetEase(Ease.InCubic))
+                .Append(DOTween.To(() => MarkerGroupCanvas.alpha, x => MarkerGroupCanvas.alpha = x, 1, (animationSpeed / 2)).SetEase(Ease.InCubic));
         }
 
     }
@@ -42,6 +50,7 @@ public class SlideInInstruction : MonoBehaviour
     public void DeactivateBlur()
     {
         DOTween.To(() => BlurBGCanvas.alpha, x => BlurBGCanvas.alpha = x, 0, (animationSpeed / 2)).SetEase(Ease.InCubic);
+        DOTween.To(() => MarkerGroupCanvas.alpha, x => MarkerGroupCanvas.alpha = x, 0, (animationSpeed / 2)).SetEase(Ease.InCubic);
         StartCoroutine(DeactiveBlurObj());
     }
 
@@ -50,5 +59,7 @@ public class SlideInInstruction : MonoBehaviour
         yield return new WaitForSeconds(animationSpeed);
         BlurBG.SetActive(false);
     }
+
+
 
 }
